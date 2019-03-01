5b8a687695610680f757eb497dc93fb0094acd24
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-03-01 18:24:13 +0800

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/**
+ * @type {webpack.Configuration}
+ */
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  devServer: {
+    port: 9200
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ]
+};
