70819bc43f207f762ab7a167aff5ab1d19f9e114
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-03-06 20:45:10 +0800

    update intial

--- pages/2.jsx
+++ pages/2.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Adder(props) {
+function Counter(props) {
   return (
     <div className="count" onClick={props.onClick}>
       Count: {props.count}
@@ -9,19 +9,12 @@ function Adder(props) {
   );
 }
 
-let count = 0;
-
-function renderApp() {
-  ReactDOM.render(
-    <Adder
-      count={count}
-      onClick={function() {
-        count++;
-        renderApp();
-      }}
-    />,
-    document.getElementById('app')
-  );
-}
-
-renderApp();
+ReactDOM.render(
+  <Counter
+    count={count}
+    onClick={function() {
+      console.log('clicked');
+    }}
+  />,
+  document.getElementById('app')
+);
